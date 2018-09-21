4848193343f9e45a03ff646692f92bb091943238
MiniMovilidad <>
2018-09-21 11:14:08 -0500

    changes

--- src/appcenter-pre-build.sh
+++ src/appcenter-pre-build.sh
@@ -1,5 +1,5 @@
 ﻿#!/usr/bin/env bash
 if [ "$APPCENTER_BRANCH" == "QA" ];
 then
-/usr/libexec/plistbuddy –c "Set : CFBundleDisplayName VSACXamarin.APPCENTER_BRANCH" "iOS/Info.plist"
+/usr/libexec/plistbuddy -c "Set :CFBundleDisplayName VSACXamarin.$APPCENTER_BRANCH" "iOS/Info.plist"
 fi
