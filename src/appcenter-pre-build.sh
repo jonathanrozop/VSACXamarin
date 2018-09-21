1473b2a0c346b26b4ef8604a5b68c609d4e69de6
MiniMovilidad <>
2018-09-21 10:42:38 -0500

    enviroment variables

--- src/appcenter-pre-build.sh
+++ src/appcenter-pre-build.sh
@@ -3,3 +3,5 @@ if [ "$APPCENTER_BRANCH" == "QA" ];
 then
 /usr/libexec/plistbuddy –c "Set : CFBundleDisplayName VSACXamarin.APPCENTER_BRANCH" "iOS/Info.plist"
 fi
+
+sed –i –e "VSAC_Keys/\[VSAC_APIKEY]/$VSAC_Key/g" Constants.cs
