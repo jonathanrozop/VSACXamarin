560a34918e8dc0ac2217ad5bee15cbdab87cd413
MiniMovilidad <>
2018-09-21 12:21:18 -0500

    bash

--- src/appcenter-pre-build.sh
+++ src/appcenter-pre-build.sh
@@ -4,10 +4,20 @@ then
 /usr/libexec/plistbuddy -c "Set :CFBundleDisplayName VSACXamarin.$APPCENTER_BRANCH" "iOS/Info.plist"
 fi
 
+echo "Start validations"
+
 if [ -n "$VSAC_Key" ]
 then
     echo "You need define the VSAC_Key variable in App center"
     exit
+fi
+
+echo "End validations"
+
+if [ -z "$VSAC_Key" ]
+then
+echo "You need define the VSAC_Key2 variable in App center"
+exit
 fi
 
 sed -i -e "s/\[VSAC_APIKEY]/$VSAC_Key/g" Constants.cs
